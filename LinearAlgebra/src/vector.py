9a458cbefd4e278d3d0f56b3513fbf42d7122e4c
Julio Castillo <=julio.castillo.2010@gmail.com>
2017-03-16 09:40:27 -0700

    First Notebook snapshots

---
+++ LinearAlgebra/src/vector.py
@@ -0,0 +1,43 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple (coordinates)
+            self.dimension = len(coordinates)
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+        except TypeError:
+            raise TypeError('The coordinates must be iterable')
+
+    def plus(self, v):
+        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+    
+    def minus(self, v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+    
+    def times_scalar(self, c):
+        new_coordinates = [c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def magnitude (self):
+        coordinates_square = [x**2 for x in self.coordinates]
+        return sqrt (sum(coordinates_squared))
+    
+    def normalized (self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1./magnitude)
+        except ZeroDivisionError:
+            raise Exception ('Cannot normalize the zero vector')
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+    
+    def __eq__(self, v):
+        return (self.coordinates == v.coordinates)
+    
+print ('Hello')
+                
