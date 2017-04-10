426d1ad3e7a618d0e1813bfce50a78c27966bf66
Julio Castillo <=julio.castillo.2010@gmail.com>
2017-04-10 09:14:45 -0700

    updated vector functions

--- LinearAlgebra/src/vector.py
+++ LinearAlgebra/src/vector.py
@@ -1,37 +1,22 @@
+from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
 class Vector(object):
+    
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple (coordinates)
+            self.coordinates = tuple ([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
         except TypeError:
             raise TypeError('The coordinates must be iterable')
-
-    def plus(self, v):
-        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
-        return Vector(new_coordinates)
-    
-    def minus(self, v):
-        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
-        return Vector(new_coordinates)
-    
-    def times_scalar(self, c):
-        new_coordinates = [c*x for x in self.coordinates]
-        return Vector(new_coordinates)
-
-    def magnitude (self):
-        coordinates_square = [x**2 for x in self.coordinates]
-        return sqrt (sum(coordinates_squared))
-    
-    def normalized (self):
-        try:
-            magnitude = self.magnitude()
-            return self.times_scalar(1./magnitude)
-        except ZeroDivisionError:
-            raise Exception ('Cannot normalize the zero vector')
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
@@ -39,5 +24,62 @@ class Vector(object):
     def __eq__(self, v):
         return (self.coordinates == v.coordinates)
     
-print ('Hello')
+    def plus (self, v):
+        new_coords = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        #new_coords = []
+        #n = len(self.coordinates)
+        #for i in range(n):
+        #    new_coords.append(self.coordinates[i] + v.coordinates[i])
+        return (Vector(new_coords))
+    
+    def minus (self, v):
+        new_coords = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return (Vector(new_coords))
+    
+    def times_scalar(self, c):
+        new_coords = [Decimal(c)*x for x in self.coordinates]
+        return (Vector(new_coords))
+    
+    def magnitude (self):
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return (Decimal(sqrt(sum(coordinates_squared))))
+    
+    def normalized (self):
+        try:
+            magnitude = self.magnitude()
+            return (self.times_scalar(Decimal('1.0')/magnitude))
+        except ZeroDivisionError:
+            raise Exception ('Cannot normalize the zero vector')
+
+    def is_zero(self, tolerance=1e-10):
+        return (self.magnitude() < tolerance)
+
+    def dot (self, v):
+        return (sum([x*y for x,y in zip(self.coordinates, v.coordinates)]))
                 
+    def angle_with(self, v, in_degrees=False):
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            dotResult = u1.dot(u2)
+            if (dotResult > 1) or (dotResult < -1):
+                dotResult = str(round(dotResult, 2))
+                dotResult = float(dotResult)
+            angle_in_radians = acos(dotResult)
+
+            if (in_degrees):
+                degrees_per_radian = 180. /pi
+                return (angle_in_radians * degrees_per_radian)
+            else:
+                return (angle_in_radians)
+        except Exception as e:
+            if (str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG):
+                raise Exception('Cannot compute an angle with the zero vector')
+            else:
+                raise (e)
+
+    def is_parallel_to(self, v):
+        return (self.is_zero() or v.is_zero() or self.angle_with(v) == 0 or self.angle_with(v) == pi)
+    
+    def is_orthogonal_to(self, v, tolerance=1e-10):
+        return (abs(self.dot(v)) < tolerance)
