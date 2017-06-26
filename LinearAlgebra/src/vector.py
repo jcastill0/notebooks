cd3f23e48e6b471923d1f4e713a0531c5b0eb0d1
Julio Castillo <jcastillo@financialengines.com>
2017-06-25 17:25:27 -0700

    new version

--- LinearAlgebra/src/vector.py
+++ LinearAlgebra/src/vector.py
@@ -77,9 +77,57 @@ class Vector(object):
                 raise Exception('Cannot compute an angle with the zero vector')
             else:
                 raise (e)
+    
+    def area_of_triangle_with(self, v):
+        return (self.area_of_parallelogram_with(v)/Decimal('2.0'))
+    
+    def area_of_parallelogram_with(self, v):
+        cross_product = self.cross(v)
+        return (cross_product.magnitude())
+    
+    def cross(self, v):
+        try:
+            x_1, y_1, z_1 = self.coordinates
+            x_2, y_2, z_2 = v.coordinates
+            new_coordinates = [ y_1*z_2 - y_2*z_1,
+                               -(x_1*z_2 - x_2*z_1),
+                               x_1*y_2 - x_2*y_1]
+            return (Vector(new_coordinates))
+        except ValueError as e:
+            msg = str(e)
+            if msg == 'need more than 2 values to unpack':
+                self_embedded_in_R3 = Vector(self.coordinates + ('0',))
+                v_embedded_in_R3 = Vector(v.coordinates + ('0',))
+                return self_embedded_in_R3.cross(v_embedded_in_R3)
+            elif (msg == 'too many values to unpack' or
+                  msg == 'need more than 1 value to unpack'):
+                raise Exception(self.ONLY_DEFINED_IN_TWO_TREE_DIMS_MSG)
+            else:
+                raise e
 
     def is_parallel_to(self, v):
         return (self.is_zero() or v.is_zero() or self.angle_with(v) == 0 or self.angle_with(v) == pi)
     
     def is_orthogonal_to(self, v, tolerance=1e-10):
         return (abs(self.dot(v)) < tolerance)
+    
+    def component_orthogonal_to(self, basis):
+        try:
+            projection = self.component_parallel_to(basis)
+            return (self.minus(projection))
+        except Exception as e:
+            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                raise Exception (self.NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG)
+            else:
+                raise e
+                
+    def component_parallel_to(self, basis):
+        try:
+            u = basis.normalized()
+            weight = self.dot(u)
+            return (u.times_scalar(weight))
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception (self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
